b98c13cfcdf262716d3bdf5948bef20ff8a240c4
U-DESKTOP-I3132OC\fairlyblank <chenxinleo@163.com>
2024-02-20 15:11:51 +0800

    use vcpkg

---
+++ env-win.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export TARGET_PLATFORM=x64-mingw-static
+
+#export CC=d:/linaro-10.2/arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-gcc.exe
+#export CXX=d:/linaro-10.2/arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-g++.exe
+#export STRIP=d:/linaro-10.2/arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-strip.exe
+#export TARGET_PLATFORM=arm-linux
